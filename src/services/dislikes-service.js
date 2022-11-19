7b5d48713f6800c75007721d876e701ce4604052
Gino <romangino@gmail.com>
2022-11-19 17:27:40 -0500

    Added findAllTuitsDislikedByUser to dislikes-service.js

--- src/services/dislikes-service.js
+++ src/services/dislikes-service.js
@@ -8,3 +8,7 @@ const api = axios.create({withCredentials: true});
 export const userTogglesTuitDislikes = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
         .then(response => response.data);
+
+export const findAllTuitsDislikedByUser = (userId) =>
+    api.get(`${USERS_API}/${userId}/dislikes`)
+        .then(response => response.data);
